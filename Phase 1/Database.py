4a2034130f555aa68df599c5b4dad96f2e15bd12
dorbi <dor.bilia2@gmila.com>
2023-10-19 14:04:21 +0300

    moved to correct DB account

--- Phase 1/Database.py
+++ Phase 1/Database.py
@@ -7,17 +7,18 @@ class Database:
     def __init__(self):
         Key = {
             "type": "service_account",
-            "project_id": "keyloggs-fa0a2",
-            "private_key_id": "773d997ea24870a7e87a5a75d2acf3487861c6d9",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCkAkS78x/wSqcD\nOXmFfzTNiIPb9hbdJhLGPKY9wtKfYmQOlkOGybldCxeRcBB9KBGt/uNu6sLsj7oa\nmzVkWLx6DV0pCJy5XuO+E1UUxfy0tSQDpC7yOLCsRJTFA4JLaCergSMUHdSWG4Ei\n01Jk9G/YwQk9ax96hBXx1vSLF/GS4jxzp4e4OPAZqa5PeCqIVYZyJ18U/WVBkMG0\nWT8ELP3NnnI4mLMhfKvq8y+fuGXiLaKcPyGN17WuzsFUOA61Cqp2FJVBZBadXPbM\nZ3z09R+9zB42hU2HsA7rYpHLtG8NGJdp0e4ljAAoObjA5OVpyt35tRo+rUVTstdu\nWGCwewO5AgMBAAECggEALRCAgWI28ugYRt8KgH9o61ao4I4YTzCwdO7iQNMg23GG\nC6oHKOiRubqcAbbYmQMjBHumU2pHIAgH+1qNS6LEOwckA2vd4GNt0WLvfFNAcgjZ\nufRpf93K0bLQa+fga8bVK59Cm7rsmEg7be3B1IKDnvu/hbRDBH95pidJr1RnaNO2\n4VK3YTSNmRSAui3NbVXy2G6OJlaUpAFcrxlP8VpNB6mHlcbmNbXGpiHcJgGY7RaJ\nzfvawy6Ym6dRkCHnKxHDAzPhfaVM6HWgrjwmwL3StmT/CQSh7Orun6HVZCTIwujF\nUQgDvvkLjg4v7ItLXc9fkPcCVN1ovIJ3JB7kRCt6zwKBgQDiEoe1I0PQ/EWYDhuW\nu+2nKVhDHDxxSlRn3o9N3DudCE4YSGRTYW45oEB1qLutfYaShmfwyAVA8/jWY7tR\n5uLrWGTlUTeejP9IJItxKpEhmO2UJ6ilRbzQkWRUbbsZPViKqiiBaxidC4SI09wK\n8Cz2fw5Rq1LeLBO3nYEt94AzMwKBgQC5uHB/SFb3z9xm9zzH9MKu77z7eYkwmqqR\nM3eZwkzMLroU3wRuA8TUUIFgrmUwkWnBrp7yhOB/X9EaOTfkDL5WsM4fIW3Vs/dz\naxgo69rMjCBcm7xoi+tqug90j+uhzU/HzWzzpfX97Ruv4F1gsI1i+ngbb+TupsbB\n1nShi3vtYwKBgBaFpBFupvXt+/zvAt6ccEuj+dANwxPCRPBoIryuAR5e0nDm3V/V\nKAcDqQhPvArHo517WGWLd8KFy7eIDgRVzSuHDd4unizkfRYx1dZ7WCQRuTF2Vf3J\nGctpgKZMnEQicWy3EPv7sGMM37JF8PkB/mi8Na/7MnFB0OENBu6OZq4zAoGAHSJy\n+3S0t/FyyPBWJ6Qc+mZ/hg+91tMcmOSzCXryo0FdTbLitgN/WDM86JbTJvhY19p1\n8uOneDPm0K9TNkJNhbn8y+NpoJG36vJM+vZs/5On6/8+YkQ47Sg9DnMZauuTbWur\ngNlzKp+MrV2MKIPp2jIir2MxTcL6ASSMbV89VLMCgYEAvKiM7plUUGYfrNkiD4IG\nYjhu/KeFqjekuu+Rl6Mg4XfH/ZKRIGZm0gC9U7qYkS2Y9sOlCchvlTWU4gKhnoG6\nMLcbvyEoG4C/kFXSe+aUPjzo94raRGl2iyMwKDElIyxrEBtmEpv7Pgi1ManEbM2q\n6/pRxYovFc01u5RJUPEmeB4=\n-----END PRIVATE KEY-----\n",
-            "client_email": "firebase-adminsdk-awu0k@keyloggs-fa0a2.iam.gserviceaccount.com",
-            "client_id": "111929317456432139111",
+            "project_id": "keyloggs-bc0f8",
+            "private_key_id": "f3f7b187ce0f0607aef8cba3bbe2ad3d6c9622cd",
+            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCfSJn5B+19tvgS\n/oYtHmrXRPouhMX41cDmszx9l4AXGS+Qdk0RNdZXTx4p7F+Hvy0x4qezxuJQGCrB\nW2EUBT6aDgq6n1z3zmUEIis2eotbIupOjbRaQDhe/CzmG1z0jeFpIMtxRuF1df8P\nhBL3XRbxhA7EJkKt7YVJrSxncTiQSxydpoXL7AKenbNBYDjcVfRRGdpcrWGoMSBO\nP5BFwSqAPvZSj1xP8zAJ/m2xiiSSRTIAnw7sre9QV181r/xaTFarUq7YAcflr6+q\naamiS7X1M4cHk3OWyJMyGe4WMyyo9Alt+WQPcyQvRj6E6MEdA47V1R/BOiRRVBvH\n9488I0PpAgMBAAECggEASD0ai61TQbkqIB8sBilt5iyj1FYmHRkAcACo9gKwNomX\nH0rv39bNW3KH6RaJKt0C58V2LlwMRqv3NBM332cTgbXNmt7swHxsJuhCNeZ07kPT\nBQZSXb4TcsympkORsF3qdY3IhprRZL67izNokKQ45BbyxXXettfqo8P0yAM61oFq\nBwQV93m0bxp1swojXapv/aZ672QQsNgFVXCc4EfROkI2gB4xzfD+vTB9gyIm46cZ\nFpOjAXnLI+z009vPWy/n4UFSFnzePMdWxPoP8qaM3XEtLnjwciI3U5spZeSXMuG3\nkbmH8gzU2W9pSBPmomuZGZznMo4CPhUjqxTyNp3PCwKBgQDgrg5rTrnTW/o8oQML\n3HVtQgGjWZsWhmk1jsodT78qOb0rFUfOnDTaTEEpTCs89hXF86YrMagTx9QUrZrf\nqAeHCfw92NuzKDtVYY+93LjXASLPzEWgi8d2rDYsdCldM9g8RqeDqTyLxX0qY60K\nE1iHFQ9zdO/P0VaduZP7Pdr5UwKBgQC1fM7EG9Qv992bbyEOzdQeg3lHRSi2YcGp\nk/NeUzu5sPJv1L3msvlvkM/aMB6R1+8KxzjhEXOo0E5DVBE+NadSW2Tfs4XMuJ4d\n4P0N182bboq2rMH7/0fYlueeiNUcZcy5zy2D6pwm86s2D5M1VyPac3u8gPUEBgkP\n0ziRB6YaUwKBgF9oymq7FaIxbsQMQ7Tnu13YJ8XsKTWZglfh+OYVlUIjHreK4+FS\n6AbDVvBojfvnLDvRSYESJyOvOdpGFnxfaELp9Jl222Tq8rdBJL8lWcDDlLrVLcQN\nV3iHjMG32lDf0TVXc48vcKySAqLbjQG4UWGbieIniCah6Lw1sCeCjthNAoGAZKMJ\nsNtRpSvIGencpe3i4uy6speaBNBeaF9fQ33aD5UcSoDosOWrxX7Ck1W9jdf0eMCS\nPNPFk+W2kEUsrpdn6hY98IIG/sec/iqFiEiTfc4lYziW9NrRBzxewZ1Ut0OH670D\nmzPcVQkndnnagmiIvBjvk07bDtkd60WQB3P7WnsCgYEA1FV5juE78CHzgD0FYvEF\nqoUmds8mFpaItUT+zPadNRlpU/zBOcIM2eK8qrZN1AN/c9+SHnPd2qqX6R6Y1KBG\nL/3ESZV/DQVHowSbquYd1UM++SFAGmNljliYCYUb4r65KrkzIJWeR/ciMU3x6z5c\nV0f+aOzcj0OkNukhP2ETx9U=\n-----END PRIVATE KEY-----\n",
+            "client_email": "firebase-adminsdk-pol05@keyloggs-bc0f8.iam.gserviceaccount.com",
+            "client_id": "111822744787625847535",
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
             "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-awu0k%40keyloggs-fa0a2.iam.gserviceaccount.com",
+            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-pol05%40keyloggs-bc0f8.iam.gserviceaccount.com",
             "universe_domain": "googleapis.com"
         }
+
         cred = credentials.Certificate(Key)
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
@@ -38,4 +39,3 @@ class Database:
     def ShowData(self, time):  # returns saved data by saving time
         data = self.db.collection("Keyloggs").document(time).get().to_dict()
         return data['data']
-
